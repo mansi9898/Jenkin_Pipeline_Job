9a6fb6d02f18e72631b841b87836c272dcbaa0ec
Mansi Gupta <mansi.gupta05@nagarro.com>
2024-05-07 13:00:57 +0530

    added jenkin project

---
+++ src/test/java/automation/mytest.java
@@ -0,0 +1,22 @@
+package automation;
+
+import org.openqa.selenium.By;
+
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class mytest {
+	@Test
+	public void test() {
+		WebDriver driver;
+		driver = new InternetExplorerDriver();
+		driver.get("http://www.amazon.in");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@class='nav-input nav-progressive-attribute']")).sendKeys("iphone 15");		
+		driver.findElement(By.id("nav-search-submit-button")).click();
+
+	}
+}
